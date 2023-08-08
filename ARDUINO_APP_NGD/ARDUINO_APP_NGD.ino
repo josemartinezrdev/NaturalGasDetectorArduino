3c83ea8dac7728a261c1d8ca5cf3f1ca9afe27c1
josemartinezrdev <josemartinezrdev@gmail.com>
2023-08-07 23:55:14 -0500

    NGD-657 BT-WIFI-Complete
    Se logró recibir los dos datos enviados desde la aplicación Android y con ellos conectar el ESP32 a wifi, además de enviar la IP del ESP32 a la aplicación la cual la recibe y deja listo para iniciar el proceso con el servidor web.

--- ARDUINO_APP_NGD/ARDUINO_APP_NGD.ino
+++ ARDUINO_APP_NGD/ARDUINO_APP_NGD.ino
@@ -4,7 +4,8 @@
 
 char ssid[16]; // Almacenar el nombre de la red, maximo 16 caracteres.
 char password[16]; // Almacenar la contraseña, maximo 16 caracteres.
-const unsigned long timeOut = 15000; // Tiempo maximo de espera (15 Seg).
+int timeInit = 0;
+int timeOut = 15000; // Tiempo maximo de espera (15 Seg).
 
 int led = 2;
 
@@ -23,7 +24,7 @@ void setup() {
 
   // Leer los datos de la aplicación Android a través de BT
   String data = SerialBT.readStringUntil('\n'); // Leer la cadena de datos hasta el salto de línea
-  Serial.println("Datos recibidos desde la aplicación Android: " + data); //Imprimir la cade de texto en consola.
+  Serial.println("Datos recibidos desde la aplicación Android: " + data); //Eliminar en fase final
 
   //Separar datos mediante una ","
 
@@ -31,8 +32,8 @@ void setup() {
   if (commaIndex != -1) {
     data.substring(0, commaIndex).toCharArray(ssid, sizeof(ssid)); // Almacenar el SSID en la variable 'ssid'
     data.substring(commaIndex + 1).toCharArray(password, sizeof(password)); // Almacenar la contraseña en la variable 'password'
-    Serial.println("SSID: " + String(ssid));
-    Serial.println("Contraseña: " + String(password));
+    Serial.println("SSID: " + String(ssid)); //Eliminar en fase final
+    Serial.println("Contraseña: " + String(password)); //Eliminar en fase final
   }
 
   timeInit = millis(); //Iniciar a contar 15 segundos
@@ -40,12 +41,12 @@ void setup() {
   // Conectar el ESP32 a la red WiFi utilizando los datos recibidos mediante BT desde Android
   WiFi.begin(ssid, password);
 
-  Serial.print("Conectando a la red WiFi...");
+  Serial.print("Conectando a la red WiFi..."); //Eliminar en fase final
   while (WiFi.status() != WL_CONNECTED) { //Verificar si esta desconectado
 
     if (millis() - timeInit >= timeOut) { //Verificar si no han pasado 15 segundos
-      Serial.println("\n¡Error! No se pudo conectar a la red WiFi en 15 segundos."); //Enviar mensaje de error
-      break;
+      Serial.println("\n¡Error! No se pudo conectar a la red WiFi en 15 segundos."); //Eliminar en fase final
+      break; //Detener el intento de conexión
     }
     delay(500);
     Serial.print(".");
@@ -53,7 +54,11 @@ void setup() {
 
   if (WiFi.status() == WL_CONNECTED) { //verificar si se conecta a wifi
     Serial.println("\nConexión WiFi establecida");
-    digitalWrite(led, HIGH);
+    Serial.println(WiFi.localIP()); //Eliminar en fase final
+    digitalWrite(led, HIGH); //Eliminar en fase final
+
+    SerialBT.println(WiFi.localIP()); //Enviando la IP a la aplicación
+
   }
 }
 
